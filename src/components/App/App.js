98d0961fe31af16cac72ac84fd63d5fabc91936d
Trevor Sansalone <trevorksansalone@gmail.com>
2020-11-08 13:12:19 -0600

    setting up routes in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Hash routes for my pages to be routed to the app.js
+import { HashRouter as Router, Route } from 'react-router-dom';
+import FeelingPage from '../Pages/FeelingPage/FeelingPage';
+import UnderstandingPage from '../Pages/UnderstandingPage/UnderstandingPage';
+import SupportPage from '../Pages/SupportPage/SupportPage';
+import CommentsPage from '../Pages/CommentsPage/CommentsPage';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+          <h4>
+            <i>Don't forget it!</i>
+          </h4>
         </header>
-        <br/>
+        <br />
       </div>
     );
   }
