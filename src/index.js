3d644a3ac96cc2549e8b6916ef941fc0b0c89232
Trevor Sansalone <trevorksansalone@gmail.com>
2020-11-08 13:33:59 -0600

    adding my default feedback page with sepperate reducers for each page then storing them and adding middleware to my app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,83 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+  bindActionCreators,
+} from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Var to store uri or url to app.js, and set a default state for each page
+const originalFeedback = {
+  feeling: '',
+  understanding: '',
+  support: '',
+  comments: '',
+};
+//
+// Reducer Functions --------------------------
+//
+
+// feedbackReducer
+function feedbackReducer(state = originalFeedback, action) {
+  if (action.type === 'GIVE_FEELING') {
+    return {
+      ...state,
+      feeling: action.payload,
+    };
+  }
+}
+
+// understandingReducer
+function understandingReducer(state = originalFeedback, action) {
+  if (action.type === 'GIVE_UNDERSTANDING') {
+    return {
+      ...state,
+      feeling: action.payload,
+    };
+  }
+}
+
+// supportReducer
+function supportReducer(state = originalFeedback, action) {
+  if (action.type === 'GIVE_SUPPORT') {
+    return {
+      ...state,
+      feeling: action.payload,
+    };
+  }
+}
+
+// commentsReducer
+function commentsReducer(state = originalFeedback, action) {
+  if (action.type === 'GIVE_COMMENTS') {
+    return {
+      ...state,
+      feeling: action.payload,
+    };
+  }
+}
+
+// Redux store
+const storeInstance = createStore(
+  // reducers
+  combineReducers({
+    feedbackReducer,
+    understandingReducer,
+    supportReducer,
+    commentsReducer,
+  }),
+  // middleware
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
